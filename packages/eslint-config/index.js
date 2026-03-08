a47fbde174d138b9c4c4f2c710a23084d03d8a89
namdaeun <nde40345@gmail.com>
2026-03-08 19:16:01 +0900

    chore: autofix 옵션 추가

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,40 +1,38 @@
 /** @type {import("eslint").Linter.Config} */
 
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
   env: {
     es6: true,
     node: true,
     browser: true,
   },
-  plugins: ["@typescript-eslint", "simple-import-sort"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier",
-  ],
+  plugins: ['@typescript-eslint', 'simple-import-sort'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   rules: {
     // TypeScript
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": [
-      "warn",
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
       {
-        varsIgnorePattern: "^_",
-        argsIgnorePattern: "^_",
-        caughtErrors: "all",
+        varsIgnorePattern: '^_',
+        argsIgnorePattern: '^_',
+        caughtErrors: 'all',
       },
     ],
-    "@typescript-eslint/no-empty-object-type": "off",
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/consistent-type-imports": "error",
-    "@typescript-eslint/consistent-type-exports": "error",
-
+    '@typescript-eslint/no-empty-object-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/consistent-type-imports': 'error',
+    '@typescript-eslint/consistent-type-exports': ['error', { fixMixedExportsWithInlineTypeSpecifier: true }],
     // Import 정렬
-    "simple-import-sort/imports": "warn",
-    "simple-import-sort/exports": "warn",
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
 
     // React Hooks
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
