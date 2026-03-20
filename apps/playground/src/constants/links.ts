4d33844734f77767662e806646f8ffc5aec9ae83
namdaeun <nde40345@gmail.com>
2026-03-20 12:01:38 +0900

    chore: 필요 없는 코드 제거

--- apps/playground/src/constants/links.ts
+++ apps/playground/src/constants/links.ts
@@ -1,5 +1,3 @@
-export { crewLink, MAKERS_TEAM_URL, playgroundLink } from '@sopt/constant';
-
 export const FEEDBACK_FORM_URL = 'https://forms.gle/FCx5WJ6mDmRuneQi9';
 export const MEMBER_REQUEST_FORM_URL = 'https://forms.gle/Hs9tJgMG9bNvT1rS9';
 export const MENTOR_APPLICATION_URL = 'https://forms.gle/iMiCSnqy5oWqAsx47';
