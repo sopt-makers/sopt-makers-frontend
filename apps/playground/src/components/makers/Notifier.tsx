0908bb9e2b4b339f181e9eb1e28d154df586a89d
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-03-24 06:15:39 +0900

    docs: 39기 모집 관련 주석 교체
    - 38기 -> 39기

--- apps/playground/src/components/makers/Notifier.tsx
+++ apps/playground/src/components/makers/Notifier.tsx
@@ -16,15 +16,15 @@ const RECRUITING_URL = 'https://sopt-makers.notion.site/38-2fd76042aac280f39548e
 const Notifier: FC<NotifierProps> = ({ className }) => {
   return (
     <StyledJoinNotifier className={className}>
-      {/* MEMO: 38기 모집 시작 시 주석 해제 */}
-      {/* <Title>현재 38기 makers 팀 모집이 진행 중이에요. 곧 만나요!</Title> */}
+      {/* MEMO: 39기 모집 시작 시 주석 해제 */}
+      {/* <Title>현재 39기 makers 팀 모집이 진행 중이에요. 곧 만나요!</Title> */}
       <Title>39기 makers 모집은 2026년 8월 중에 진행될 예정이에요.</Title>
-      {/* <SubTitle>38기 모집은 2026년 2월 6일 금요일부터 2월 13일 금요일 23:59까지 진행될 예정이에요.</SubTitle> */}
+      {/* <SubTitle>39기 모집은 2026년 2월 6일 금요일부터 2월 13일 금요일 23:59까지 진행될 예정이에요.</SubTitle> */}
       <ButtonGroup>
-        {/* MEMO: 36기 모집 알림 신청시에 다시 주석 해제 */}
+        {/* MEMO: 39기 모집 알림 신청시에 다시 주석 해제 */}
         {/* <SubscribeButton href={RECRUIT_NOTIFY_GENERATION_URL} target='_blank'>
           <StyledBellIcon />
-          36기 모집 알림 신청
+          39기 모집 알림 신청
         </SubscribeButton> */}
         <ExpiredButton href={RECRUITING_URL} target='_blank'>
           <StyledOutgoingIcon />
