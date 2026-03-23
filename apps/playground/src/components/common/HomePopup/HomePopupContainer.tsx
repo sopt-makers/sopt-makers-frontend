88a2f1be6c80c528e68ac0acc4e4b8e2a3a6b394
jogpfls <joerin4177@gmail.com>
2026-03-23 20:10:41 +0900

    feat: api 반영 이전 임시 타임캡솝 적용

--- apps/playground/src/components/common/HomePopup/HomePopupContainer.tsx
+++ apps/playground/src/components/common/HomePopup/HomePopupContainer.tsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from 'react';
-
 import { useGetHomePopup } from '@/api/endpoint/homePopup/getHomePopup';
 import { useGetMemberOfMe } from '@/api/endpoint/members/getMemberOfMe';
 import { HomePopup } from '@/components/common/HomePopup';
-import useModalState from '@/components/common/Modal/useModalState';
-import { useMatchMemberEvent } from '@/components/matchmember/hooks/useMatchMemberEvent';
-import MatchMemberModal from '@/components/matchmember/MatchMemberModal';
 import { LATEST_GENERATION } from '@/constants/generation';
 
 const HomePopupContainer = () => {
   const { data: myData } = useGetMemberOfMe();
-  const { data: homePopupData } = useGetHomePopup();
+  // const { data: homePopupData } = useGetHomePopup();
   const isLastGeneration = myData?.generation === LATEST_GENERATION;
 
-  if (!homePopupData || typeof homePopupData === 'string') {
-    return null;
-  }
+  // if (!homePopupData || typeof homePopupData === 'string') {
+  //   return null;
+  // }
 
-  const { startDate, endDate, pcImageUrl, mobileImageUrl, linkUrl, openInNewTab, showOnlyToRecentGeneration } =
-    homePopupData;
+  // const { startDate, endDate, pcImageUrl, mobileImageUrl, linkUrl, openInNewTab, showOnlyToRecentGeneration } =
+  //   homePopupData;
 
   // 팝업 표시 기간 설정
   const now = new Date();
-  const popupStart = new Date(`${startDate}T00:00:00+09:00`);
-  const popupEnd = new Date(`${endDate}T23:59:59+09:00`);
+  const popupStart = new Date(`2026-03-23T00:00:00+09:00`);
+  const popupEnd = new Date(`2026-04-11T23:59:59+09:00`);
   const isWithinPeriod = now >= popupStart && now <= popupEnd;
 
   //팝업 표시 기간이 아닌 경우
@@ -32,19 +27,19 @@ const HomePopupContainer = () => {
   }
 
   //최신 기수만 보기 옵션이 활성화인 경우
-  if (showOnlyToRecentGeneration && !isLastGeneration) {
+  if (!isLastGeneration) {
     return null;
   }
 
   return (
     <>
-      <HomePopup
+      {/* <HomePopup
         pcImageUrl={pcImageUrl}
         mobileImageUrl={mobileImageUrl}
         linkUrl={linkUrl}
         openInNewTab={openInNewTab}
-      />
-      {/* <HomePopup pcImageUrl='/icons/img/popup/PC.png' mobileImageUrl='/icons/img/popup/MO.png' openInNewTab={false} /> */}
+      /> */}
+      <HomePopup pcImageUrl='/icons/img/popup/PC.png' mobileImageUrl='/icons/img/popup/MO.png' openInNewTab={false} />
     </>
   );
 };
