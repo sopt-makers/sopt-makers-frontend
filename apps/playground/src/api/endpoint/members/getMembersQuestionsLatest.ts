d400a554b160881a9f9324de5d34ec3322eb6dc0
jogpfls <joerin4177@gmail.com>
2026-04-16 22:13:27 +0900

    feat: 최신 질문 가져오기 api 구현

---
+++ apps/playground/src/api/endpoint/members/getMembersQuestionsLatest.ts
@@ -0,0 +1,35 @@
+import { useQuery } from '@tanstack/react-query';
+import { z } from 'zod';
+
+import { createEndpoint } from '@/api/typedAxios';
+
+const getMembersQuestionsLatest = createEndpoint({
+  request: () => ({
+    method: 'GET',
+    url: 'api/v1/members/questions/latest',
+  }),
+  serverResponseScheme: z.object({
+    questions: z.array(
+      z.object({
+        receiverId: z.number(),
+        receiverName: z.string(),
+        receiverProfileImage: z.string().nullable(),
+        questionId: z.number(),
+        content: z.string(),
+        location: z.object({
+          questionId: z.number(),
+          tab: z.literal('answered'),
+          page: z.number(),
+          index: z.number(),
+        }),
+      }),
+    ),
+  }),
+});
+
+export const useGetMembersQuestionsLatest = () => {
+  return useQuery({
+    queryKey: ['getMembersQuestionsLatest'],
+    queryFn: () => getMembersQuestionsLatest.request(),
+  });
+};
