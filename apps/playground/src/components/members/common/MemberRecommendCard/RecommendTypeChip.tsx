82095630c462f46a6ea541eb7a36070d37160226
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-04-20 23:12:49 +0900

    feat: 추천 타입 칩 컴포넌트 구현

---
+++ apps/playground/src/components/members/common/MemberRecommendCard/RecommendTypeChip.tsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import { colors } from '@sopt-makers/colors';
+import { fonts } from '@sopt-makers/fonts';
+
+import type { RecommendMemberById } from '@/api/endpoint/members/getMemberRecommendById';
+import type { RecommendMemberOfMe } from '@/api/endpoint/members/getMemberRecommendOfMe';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
+
+type RecommendType = RecommendMemberOfMe['recommendType'] | RecommendMemberById['recommendType'];
+
+const RECOMMEND_TYPE_LABEL: Record<RecommendType, string> = {
+  SAME_PART: '같은 파트 🛠️',
+  SAME_CREW: '같은 모임 👥',
+  SAME_MBTI: '같은 MBTI 🧬',
+  SAME_UNIVERSITY: '같은 학교 🏫',
+  SAME_GENERATION: '같은 기수 🔢',
+  SAME_PROJECT: '같은 프로젝트 💡',
+};
+
+interface RecommendTypeChipProps {
+  recommendType: RecommendType;
+}
+
+const RecommendTypeChip = ({ recommendType }: RecommendTypeChipProps) => {
+  return <Container>{RECOMMEND_TYPE_LABEL[recommendType]}</Container>;
+};
+
+export default RecommendTypeChip;
+
+const Container = styled.div`
+  padding: 5px 5px 5px 8px;
+  background-color: #fdbbf9;
+  border-radius: 20px;
+  color: ${colors.black};
+  white-space: nowrap;
+  ${fonts.LABEL_12_SB}
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    font-size: 10px;
+    line-height: 12px;
+  }
+`;
