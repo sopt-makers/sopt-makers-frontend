9e86198c7da721b54a74646acd11f2a47eb42792
조혜린 <164613169+jogpfls@users.noreply.github.com>
2026-04-29 15:12:46 +0900

    fix: 에스크 카드 QA 반영 (#56)
    * fix: 검색 필터링 운영팀 순서 변경
    
    * fix: padding 스타일 수정
    
    * fix: 스크롤 시 레이아웃 깨짐 현상 해결
    
    * feat: 툴팁 말풍선 추가
    
    * chore: 주석에 TODO 작성
    
    * fix: PC_MEDIA_WIDTH 값을 1920에서 1200으로 수정

--- apps/playground/src/components/members/main/MemberRecommendSection/MemberRecommendSection.tsx
+++ apps/playground/src/components/members/main/MemberRecommendSection/MemberRecommendSection.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
+import * as Tooltip from '@radix-ui/react-tooltip';
 import { playgroundLink } from '@sopt/constant';
 import { colors } from '@sopt-makers/colors';
 import { fonts } from '@sopt-makers/fonts';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 import { useGetMemberRecommendOfMe } from '@/api/endpoint/members/getMemberRecommendOfMe';
 import RefreshIcon from '@/public/icons/icon_refresh.svg';
@@ -11,14 +13,55 @@ import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 import MemberRecommendCard from '../../common/MemberRecommendCard/MemberRecommendCard';
 import { DESKTOP_TWO_MEDIA_QUERY } from '../contants';
 
+const PC_MEDIA_WIDTH = 1200;
+const PC_MEDIA_QUERY = `screen and (min-width: ${PC_MEDIA_WIDTH}px)`;
+
 const MemberRecommendSection = () => {
   const { data: memberRecommendData, refetch } = useGetMemberRecommendOfMe();
+  const [isTooltipOpen, setIsTooltipOpen] = useState(true);
+  const [isWideViewport, setIsWideViewport] = useState(false);
+
+  // TODO: viewpoint 판단은 추후 상위 컴포넌트로 이동
+  useEffect(() => {
+    const media = window.matchMedia(PC_MEDIA_QUERY);
+    setIsWideViewport(media.matches);
+
+    const handleChange = (e: MediaQueryListEvent) => setIsWideViewport(e.matches);
+    media.addEventListener('change', handleChange);
+    return () => media.removeEventListener('change', handleChange);
+  }, []);
+
+  const handleRefreshClick = () => {
+    setIsTooltipOpen(false);
+    refetch();
+  };
 
   return (
     <StyledSection>
       <StyledSectionHeader>
         <StyledSectionTitle>나와 접점이 있는 멤버</StyledSectionTitle>
-        <StyledRefreshIcon onClick={refetch} />
+        {isWideViewport ? (
+          //TODO: 추후 디자인 시스템 툴팁으로 변경
+          <Tooltip.Provider>
+            <Tooltip.Root open={isTooltipOpen}>
+              <Tooltip.Trigger asChild>
+                <button onClick={handleRefreshClick}>
+                  <StyledRefreshIcon />
+                </button>
+              </Tooltip.Trigger>
+              <Tooltip.Portal>
+                <TooltipContent side='top' avoidCollisions={false}>
+                  더 많은 멤버를 찾아보세요!
+                  <TooltipArrow />
+                </TooltipContent>
+              </Tooltip.Portal>
+            </Tooltip.Root>
+          </Tooltip.Provider>
+        ) : (
+          <button onClick={handleRefreshClick}>
+            <StyledRefreshIcon />
+          </button>
+        )}
       </StyledSectionHeader>
       <StyledCardGrid>
         {memberRecommendData?.members.map((member) => (
@@ -82,6 +125,30 @@ const StyledRefreshIcon = styled(RefreshIcon)`
     width: 24px;
     height: 24px;
   }
+`;
+
+const TooltipContent = styled(Tooltip.Content)`
+  position: relative;
+  padding: 12px 14px;
+  border-radius: 12px;
+  background-color: ${colors.gray600};
+  margin: 8px;
+  ${fonts.BODY_13_M}
+  box-shadow:
+    0 1px 4px 0 rgba(12, 12, 13, 0.05),
+    0 1px 4px 0 rgba(12, 12, 13, 0.1);
+`;
+
+const TooltipArrow = styled.div`
+  position: absolute;
+  bottom: -9px;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 0;
+  height: 0;
+  border-left: 7px solid transparent;
+  border-right: 7px solid transparent;
+  border-top: 12px solid ${colors.gray600};
 `;
 
 const StyledCardGrid = styled.div`
