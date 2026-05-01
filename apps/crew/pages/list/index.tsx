d9b98f8cc50cd6c5e6c214c35e85c1ea38e01750
JeongHoon <113702672+jeonghoon11@users.noreply.github.com>
2026-05-02 00:57:07 +0900

    fix: 이벤트 배너 다시 올리기 (#81)

--- apps/crew/pages/list/index.tsx
+++ apps/crew/pages/list/index.tsx
@@ -22,9 +22,9 @@ const Home: NextPage = () => {
         </SFilterWrapper>
 
         {/*이벤트 배너 - 이벤트 신청 안내 배너, 노출 여부는 API에서 받아오는 데이터에 따라 결정*/}
-        {/* <SEventPeriodBannerWrapper>
+        <SEventPeriodBannerWrapper>
           <EventPeriodBanner />
-        </SEventPeriodBannerWrapper> */}
+        </SEventPeriodBannerWrapper>
 
         {/*모임 목록들 - MeetingListOfAll : 내부적으로 쿼리 파라미터 이용하여 필터링 적용*/}
         <MeetingListOfAll />
