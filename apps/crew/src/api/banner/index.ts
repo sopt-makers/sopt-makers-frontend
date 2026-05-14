4bcbd546a598245c42702ec470abc28b05837800
constantly-dev <rntqhdl10@naver.com>
2026-05-14 19:33:22 +0900

    hotfix: 배포 플로우 operation env 설정 추가

--- apps/crew/src/api/banner/index.ts
+++ apps/crew/src/api/banner/index.ts
@@ -3,7 +3,7 @@ import type { GetBannersResponse } from '@api/banner/type';
 import { operationApi } from '..';
 
 export const getBanners = async (location: string) => {
-  const { data } = await operationApi.get<GetBannersResponse>('/banners/images', {
+  const { data } = await operationApi.get<GetBannersResponse>('/api/v1/banners/images', {
     params: { location },
   });
   return data;
