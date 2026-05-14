863534fd897bcc92f2c973f66133e34489f272f6
constantly-dev <rntqhdl10@naver.com>
2026-05-14 11:41:38 +0900

    feat: getEventBannerInfo에 eventType 파라미터 추가

--- apps/crew/src/api/advertisement/type.ts
+++ apps/crew/src/api/advertisement/type.ts
@@ -7,3 +7,5 @@ export type AdvertisementCategoryType = 'POST' | 'MEETING';
 
 export type GetEventPeriodBanner =
   paths['/advertisement/v2/meeting/top']['get']['responses']['200']['content']['application/json;charset=UTF-8'];
+
+export type EventBannerEventType = paths['/advertisement/v2/meeting/top']['get']['parameters']['query']['eventType'];
