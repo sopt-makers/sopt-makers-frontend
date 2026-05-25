2458d77ed5b4ae4e1e12de6fe6a3545eb5bafac5
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-05-26 04:03:09 +0900

    refactor: borderRadius 값 문자열로 변경

--- apps/playground/src/components/wordchain/WordchainEntry/WordchainSkeleton.tsx
+++ apps/playground/src/components/wordchain/WordchainEntry/WordchainSkeleton.tsx
@@ -6,13 +6,13 @@ import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 
 const WordchainSkeleton = () => {
   return (
-    <SkeletonWrapper borderRadius={14} height={60}>
-      <SkeletonResponsive1 width={28} height={28} borderRadius={8} color={colors.gray700} margin='0 8px 0 0' />
+    <SkeletonWrapper borderRadius={'14px'} height={60}>
+      <SkeletonResponsive1 width={28} height={28} borderRadius={'8px'} color={colors.gray700} margin='0 8px 0 0' />
       <MiddleWrapper>
-        <SkeletonResponsive2 width={47} height={20} borderRadius={8} color={colors.gray700} margin='0 16px 0 0' />
-        <SkeletonResponsive3 width={286} height={20} borderRadius={8} color={colors.gray700} />
+        <SkeletonResponsive2 width={47} height={20} borderRadius={'8px'} color={colors.gray700} margin='0 16px 0 0' />
+        <SkeletonResponsive3 width={286} height={20} borderRadius={'8px'} color={colors.gray700} />
       </MiddleWrapper>
-      <SkeletonResponsive4 width={28} height={28} borderRadius={8} color={colors.gray700} margin='0 0 0 115px' />
+      <SkeletonResponsive4 width={28} height={28} borderRadius={'8px'} color={colors.gray700} margin='0 0 0 115px' />
     </SkeletonWrapper>
   );
 };
