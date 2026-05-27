76810191f16edfc23ff42122233aae710007f962
jogpfls <joerin4177@gmail.com>
2026-05-27 17:50:01 +0900

    feat: SopticleArea 컴포넌트를 제거 및 StyledContainer로 감싸기

--- apps/playground/src/components/feed/home/index.tsx
+++ apps/playground/src/components/feed/home/index.tsx
@@ -1,16 +1,28 @@
+import styled from '@emotion/styled';
+
 import RecentArea from '@/components/feed/home/RecentArea';
-import SopticleArea from '@/components/feed/home/SopticleArea';
+import { MOBILE_MEDIA_QUERY } from '@/styles/mediaQuery';
 
 import ReactionArea from './ReactionArea';
 
 const Hot = () => {
   return (
-    <>
+    <StyledContainer>
       <RecentArea />
       <ReactionArea />
-      <SopticleArea />
-    </>
+    </StyledContainer>
   );
 };
 
 export default Hot;
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+  margin-bottom: 120px;
+
+  @media ${MOBILE_MEDIA_QUERY} {
+    gap: 28px;
+  }
+`;
