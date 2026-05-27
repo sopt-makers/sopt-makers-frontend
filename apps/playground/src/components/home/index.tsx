d44ab3f2d691cc53f914df2353d2f19c6b32fae4
jogpfls <joerin4177@gmail.com>
2026-05-27 17:28:16 +0900

    fix: 충돌 해결

--- apps/playground/src/components/home/index.tsx
+++ apps/playground/src/components/home/index.tsx
@@ -4,7 +4,6 @@ import { DESKTOP_ONE_MEDIA_QUERY, DESKTOP_TWO_MEDIA_QUERY, MOBILE_MEDIA_QUERY } 
 
 import WordChainEntry from '../wordchain/WordchainEntry/WordChainEntry';
 import MenuPreview from './common/MenuPreview';
-import PopularArea from './PopularArea';
 import TitledContent from './common/TitledContent';
 import PopularArea from './PopularArea';
 import ProjectList from './ProjectPreview/ProjectList';
