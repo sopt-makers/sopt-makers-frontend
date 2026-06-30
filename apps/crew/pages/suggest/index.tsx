93dac6ba0c0e2727af879181cfc7cf9cc858591f
jeonghoon11 <jih361@naver.com>
2026-06-30 20:49:00 +0900

    feat: 모임 제안 페이지 제출 폼 구성

---
+++ apps/crew/pages/suggest/index.tsx
@@ -0,0 +1,69 @@
+import AdditionalInfoSection from '@domain/suggest/AdditionalInfoSection';
+import Header from '@domain/suggest/Header';
+import RequiredInfoSection from '@domain/suggest/RequiredInfoSection';
+import type { SuggestFormValues } from '@domain/suggest/schema';
+import { suggestFormSchema } from '@domain/suggest/schema';
+import SubmitButton from '@domain/suggest/SubmitButton';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { spacingBase } from '@sopt-mds/design-tokens';
+import { FormProvider, useForm } from 'react-hook-form';
+import { styled } from 'stitches.config';
+
+const SuggestMeetingPage = () => {
+  const formMethods = useForm<SuggestFormValues>({
+    mode: 'onChange',
+    reValidateMode: 'onChange',
+    resolver: zodResolver(suggestFormSchema),
+    defaultValues: {
+      title: '',
+      expectation: '',
+      topics: [],
+      participationMethod: '',
+      participationLevel: '',
+    },
+  });
+  const handleSubmit = formMethods.handleSubmit((formValues) => {
+    // @TODO: 모임 제안 API 연동
+    void formValues;
+  });
+
+  return (
+    <FormProvider {...formMethods}>
+      <SPage>
+        <Header />
+
+        <SForm onSubmit={handleSubmit}>
+          <RequiredInfoSection />
+          <AdditionalInfoSection />
+          <SubmitButton />
+        </SForm>
+      </SPage>
+    </FormProvider>
+  );
+};
+
+export default SuggestMeetingPage;
+
+const SPage = styled('main', {
+  'width': '100%',
+  'maxWidth': '1120px',
+  'margin': `${spacingBase.s80} auto`,
+
+  '@desktop': {
+    maxWidth: '704px',
+  },
+
+  '@tablet': {
+    maxWidth: '688px',
+  },
+
+  '@mobile': {
+    maxWidth: 'none',
+    margin: `${spacingBase.s32} auto`,
+  },
+});
+
+const SForm = styled('form', {
+  display: 'flex',
+  flexDirection: 'column',
+});
---
+++ apps/crew/src/domain/suggest/SubmitButton/index.tsx
@@ -0,0 +1,41 @@
+import { useDisplay } from '@hook/useDisplay';
+import { spacingBase } from '@sopt-mds/design-tokens';
+import { ActionButton } from '@sopt-mds/ui';
+import { useFormContext } from 'react-hook-form';
+import { styled } from 'stitches.config';
+
+import type { SuggestFormValues } from '../schema';
+
+const SubmitButton = () => {
+  const { isMobile } = useDisplay();
+  const {
+    formState: { isSubmitting, isValid },
+  } = useFormContext<SuggestFormValues>();
+  const isDisabled = isSubmitting || !isValid;
+
+  return (
+    <SButtonContainer>
+      <ActionButton type='submit' size='large' variant='primary' disabled={isDisabled}>
+        {isMobile ? '제안하기' : '모임 제안하기'}
+      </ActionButton>
+    </SButtonContainer>
+  );
+};
+
+export default SubmitButton;
+
+const SButtonContainer = styled('div', {
+  'display': 'flex',
+  'justifyContent': 'flex-end',
+
+  'marginTop': spacingBase.s64,
+
+  '@mobile': {
+    'justifyContent': 'center',
+    'marginTop': '61px',
+
+    '& button': {
+      width: '320px',
+    },
+  },
+});
