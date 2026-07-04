e74d7d7e8cf9e8e902646018b5925d0abf52c984
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-07-04 22:48:38 +0900

    chore: 테스트용 MemberBlock 제거

--- apps/playground/src/components/projects/main/ProjectDetail.tsx
+++ apps/playground/src/components/projects/main/ProjectDetail.tsx
@@ -180,10 +180,6 @@ const ProjectDetail = ({ projectId }: ProjectDetailProps) => {
                 </Link>
               );
             })}
-            <MemberBlock name='조혜린' position='Front-end' imageUrl='' badges={['38기']} />
-            <MemberBlock name='조혜린' position='Front-end' imageUrl='' badges={['38기']} />
-            <MemberBlock name='조혜린' position='Front-end' imageUrl='' badges={['38기']} />
-            <MemberBlock name='조혜린' position='Front-end' imageUrl='' badges={['38기']} />
           </UserList>
         </UserWrapper>
       </ProjectDetailContainer>
