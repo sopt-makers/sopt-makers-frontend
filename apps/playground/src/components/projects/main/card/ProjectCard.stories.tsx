b4e083fcb6780c3c8b08132973cdc8e344d7f1c5
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-07-05 03:04:41 +0900

    refactor: ProjectCard 스토리 변경된 구조에 맞게 수정

--- apps/playground/src/components/projects/main/card/ProjectCard.stories.tsx
+++ apps/playground/src/components/projects/main/card/ProjectCard.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import type { Project } from '@/api/endpoint/projects/getProjects';
+
 import ProjectCard from './ProjectCard';
 
 const meta = {
@@ -14,20 +16,46 @@ const 프로젝트_이미지 = {
     'https://github.com/sopt-makers/sopt-playground-frontend/assets/26808056/392e5423-06b0-4b50-ac39-6cd51449536a',
 };
 
+const 프로젝트: Project = {
+  id: 1,
+  name: 'TATTOUR (타투어)',
+  generation: 37,
+  category: 'SOPKATHON',
+  serviceType: ['APP', 'WEB'],
+  isAvailable: false,
+  isFounding: false,
+  summary:
+    '후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR\n 후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR 후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR \n후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR',
+  detail: '',
+  logoImage: 프로젝트_이미지.타투어,
+  thumbnailImage: 프로젝트_이미지.타투어,
+  links: [],
+};
+
 export const Default = {
   args: {
-    image: 프로젝트_이미지.타투어,
-    title: 'TATTOUR (타투어)',
-    serviceType: ['APP', 'WEB'],
-    summary:
-      '후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR\n 후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR 후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR \n후회없는 선택의 여정을 함께, 커스텀 타투 체험 서비스 TATTOUR',
+    project: 프로젝트,
   },
 } satisfies Story;
 
 export const 창업_및_이용가능 = {
   args: {
-    ...Default.args,
-    isAvailable: true,
-    isFounding: true,
+    project: {
+      ...프로젝트,
+      isAvailable: true,
+      isFounding: true,
+    },
+  },
+} satisfies Story;
+
+export const 링크_있음 = {
+  args: {
+    project: {
+      ...프로젝트,
+      links: [
+        { linkId: 1, linkTitle: 'website', linkUrl: 'https://sopt.org' },
+        { linkId: 2, linkTitle: 'github', linkUrl: 'https://github.com/sopt-makers' },
+      ],
+    },
   },
 } satisfies Story;
