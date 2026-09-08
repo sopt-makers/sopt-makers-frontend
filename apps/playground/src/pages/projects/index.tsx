636e58257b32c4a51909bb5188c3f82f48c4d9bf
sonnnnhe <147504410+sonnnnhe@users.noreply.github.com>
2026-09-08 21:05:21 +0900

    feat: 타임캡솝 배너 노출 종료 및 관련 팝업 코드 제거 (#192)
    * chore: 임시 팝업 코드 제거
    
    * chore: 타임캡솝 배너 -> 기본 배너 노출

--- apps/playground/src/pages/projects/index.tsx
+++ apps/playground/src/pages/projects/index.tsx
@@ -1,13 +1,10 @@
 import AuthRequired from '@/components/auth/AuthRequired';
-import HomePopupContainer from '@/components/common/HomePopup/HomePopupContainer';
 import ProjectList from '@/components/projects/main/ProjectList';
 import { setLayout } from '@/utils/layout';
 
 const ProjectPage = () => {
   return (
     <AuthRequired>
-      {/* TODO: 타임캡솝 닫을 때 팝업 코드 제거 */}
-      <HomePopupContainer />
       <ProjectList />
     </AuthRequired>
   );
